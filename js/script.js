87bc101ca7695e3bede8104706fee16a3a3f503c
Benja307 <benjacorujo15@gmail.com>
2023-10-24 16:20:29 -0300

    header mobile listo

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const close = document.querySelector("#close");
+const menu = document.querySelector("#menu");
+const nav_bar = document.querySelector("#nav-bar");
+const cart = document.querySelector("#cart");
+const popup = document.querySelector("#carro");
+
+menu.addEventListener("click",()=>{
+    nav_bar.style.display = "flex";
+})
+
+close.addEventListener("click",()=>{
+    nav_bar.style.display = "none";
+})
+
+cart.addEventListener("click",()=>{
+    if(popup.style.display === ""){
+        popup.style.display = "flex";
+    }else if(popup.style.display === "flex"){
+        popup.style.display = "";
+    }
+})
